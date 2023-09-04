01162e21dde8789d9f1681e3ac1277b80b2f7a07
AlesyaLipskaya <l1pskayaalesya@yandex.by>
2023-09-04 21:53:38 +0300

    22

---
+++ src/main/java/work4/w3.java
@@ -0,0 +1,11 @@
+package work4;
+
+public class w3 {
+    public static void main(String[] args) {
+        canner in = new Scanner(System.in);
+    System.out.println("введите число ");
+    int lengthArray = scan.nexInt();
+double[] masDouble = new double[lengthArray]
+
+    }
+}
---
+++ src/main/java/work4/w4.java
@@ -0,0 +1,11 @@
+package work4;
+public class w4 {
+    public static void main (String[] args) {
+        for(int i = 0; i < 5;i++) {
+        int a = 3 + (int) ( Math.random() * 6 );
+            int a1 = a;
+            int b1 = 2;
+            System.out.printf("a %.0f \n", Math.pow(a1, b1));
+    }
+    }
+}
