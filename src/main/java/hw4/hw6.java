7830f82198bd88a7c1cc0295bba3aca608ff0d96
AlesyaLipskaya <l1pskayaalesya@yandex.by>
2023-09-07 16:04:23 +0300

    22

--- src/main/java/hw4/hw6.java
+++ src/main/java/hw4/hw6.java
@@ -3,10 +3,10 @@ import java.util.Arrays;
 public class hw6 {
     public static void main(String[] args) {
         System.out.println( "6 задание" );
-    int[] task_6 = {1, 2, 3, 4, 5};
+    int[] z6 = {1, 2, 3, 4, 5};
     boolean arg = true;
-        for (int i = 1; i < task_6.length; i++) {
-        if (task_6[i] <= task_6[i - 1]) {
+        for (int i = 1; i < z6.length; i++) {
+        if (z6[i] <= z6[i - 1]) {
             arg = false;
             break;
         }
