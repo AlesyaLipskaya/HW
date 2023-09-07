5f97463b1065d6b7132880732e345c6f020359c8
AlesyaLipskaya <l1pskayaalesya@yandex.by>
2023-09-07 16:05:18 +0300

    22

--- src/main/java/hw4/hw5.java
+++ src/main/java/hw4/hw5.java
@@ -3,16 +3,16 @@ import java.util.Arrays;
 public class hw5 {
     public static void main(String[] args) {
         System.out.println( "5 задание" );
-        int[] task_5 = new int[8];
+        int[] z5 = new int[8];
         for (int i = 0; i < 8; i++) {
-            task_5[i] = (int) (Math.random() * 15 - 8);
+            z5[i] = (int) (Math.random() * 15 - 8);
         }
-        System.out.println(Arrays.toString(task_5));
+        System.out.println(Arrays.toString(z5));
         int y;
         int x;
-        for (y = 0, x = task_5.length-1; y < task_5.length && x >= 0; y++, x--) {
-            task_5[y] = task_5[x];
+        for (y = 0, x = z5.length-1; y < z5.length && x >= 0; y++, x--) {
+            z5[y] = z5[x];
         }
-        System.out.println(Arrays.toString(task_5));
+        System.out.println(Arrays.toString(z5));
 }
 }
