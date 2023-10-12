ba38933f887df1c058f16e9dd5ec19e7ace12cb2
AlesyaLipskaya <l1pskayaalesya@yandex.by>
2023-10-12 17:06:42 +0300

    22

--- src/main/java/hw12/FileAnalyzer.java
+++ src/main/java/hw12/FileAnalyzer.java
@@ -4,24 +4,30 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 public class FileAnalyzer {
-    public static void main(String[] args) {
-        String inputFile = "D:\\java\\idea\\HW\\src\\main\\java\\hw12\\input.txt";
-        String outputFile = "D:\\java\\idea\\HW\\src\\main\\java\\hw12\\output.txt";
-        try {
-            Scanner scanner = new Scanner(inputFile);
-            String longestWord = "";
-            while (scanner.hasNext()) {
-                String word = scanner.next();
-                if (word.length() > longestWord.length()) {
-                    longestWord = word;
+        public static void main(String[] args) {
+            File inputFile = new File("D:\\java\\idea\\HW\\src\\main\\java\\hw12\\input.txt");
+            try {
+                Scanner scanner = new Scanner(inputFile);
+                String longestWord = "";
+                while (scanner.hasNext()) {
+                    String word = scanner.next();
+                    if (word.length() > longestWord.length()) {
+                        longestWord = word;
+                    }
                 }
+                scanner.close();
+
+                File outputFile = new File("D:\\java\\idea\\HW\\src\\main\\java\\hw12\\output.txt");
+                try {
+                    PrintWriter writer = new PrintWriter(outputFile);
+                    writer.println("Longest word: " + longestWord);
+                    writer.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
             }
-            PrintWriter writer = new PrintWriter(outputFile);
-            writer.println("Самое длинное слово: " + longestWord);
-            writer.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Файл не найден: " + e.getMessage());
         }
     }
-}
 
