eff69b9c5ec272473986755affe6b6c3396cbd2e
AlesyaLipskaya <l1pskayaalesya@yandex.by>
2023-10-26 20:26:28 +0300

    22

--- src/main/java/hw15/Main.java
+++ src/main/java/hw15/Main.java
@@ -15,20 +15,17 @@ public class Main {
         numbers.add(4);
         numbers.add(5);
 
-        // Удаление дубликатов
         List<Integer> distinctNumbers = numbers.stream()
                 .distinct()
                 .collect(Collectors.toList());
         System.out.println("Удаление дубликатов: " + distinctNumbers);
 
-        // Оставить только четные элементы
         List<Integer> evenNumbers = numbers.stream()
                 .distinct()
                 .filter(n -> n % 2 == 0)
                 .collect(Collectors.toList());
         System.out.println("Оставить только четные элементы: " + evenNumbers);
 
-        // Сумма оставшихся элементов
         int sum = numbers.stream()
                 .distinct()
                 .filter(n -> n % 2 == 0)
